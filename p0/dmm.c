a9c8f14becf15bed613a5b58f2f366056ff98b1b
MARK SCHREIBER <mas128@duke.edu>
2018-02-10 11:52:35 -0500

    passing all test cases version

--- p0/dmm.c
+++ p0/dmm.c
@@ -50,88 +50,9 @@ metadata_t *freelist = NULL;
 metadata_t *lastVisited = NULL;
 void *bp0 = NULL;
 
-//static metadata_t* freelist = NULL; /* THE INITIAL FREE LIST, WITH MAX_HEAP_SIZE AS THE SIZE CLASS*/
 
 
-
-// Very efficient bitwise round of sz up to nearest multiple of mult
-// does this by adding mult-1 to sz, then masking off the
-// the bottom bits to compute least multiple of mult that is
-// greater/equal than sz, this value is returned
-// NOTE: mult has to be power of 2 for the bitwise trick to work!
-static inline size_t roundup(size_t sz, int mult)
-{
-  return (sz + mult-1) & ~(mult-1);
-}
-
-
-// Given a pointer to start of payload, simply back up
-// to access its block header
-static inline metadata_t *hdr_for_payload(void *payload)
-{
-  return (metadata_t *)((char *)payload - sizeof(metadata_t));
-}
-
-// Given a pointer to block header, advance past
-// header to access start of payload
-static inline void *payload_for_hdr(metadata_t *header)
-{
-  return (char *)header + sizeof(metadata_t);
-}
-
-// /* Function: get_right_list
-//  * ------------------------
-//  * Finds and returns the index of the list where a 
-//  * free node of size "size" would be stored
-//  */
-// static inline int get_right_list(size_t size){
-//   //Number of binary digits up to the msb that is on. Offset by four because list one starts at 2^3
-//   return(32 - __builtin_clz(size) - 4);
-// }
-
-// /* Function: add_to_list
-//  * ---------------------
-//  * Takes in a metadata_t and a list index that corresponds to
-//  * the list it is in.
-//  * Appends the node to the begging of the list 
-//  */
-// static void add_to_list(metadata_t *node, int listIndex){
-//   node->next = list_arr[listIndex];
-//   node->prev = NULL;
-//   if(list_arr[listIndex]) list_arr[listIndex]->prev = node;
-//   list_arr[listIndex] = node;
-// }
-
-// /* Function: remove_from_list
-//  * --------------------------
-//  * Takes in a metadata_t and a list index that corresponds to
-//  * the list it is in.
-//  * Removes the node from its list.
-//  */
-// static void remove_from_list(metadata_t *node, int listIndex){
-//   if(!node->prev && !node->next) list_arr[listIndex] = NULL; // if it is the only one
-//   if(node->prev) node->prev->next = node->next;
-//   else list_arr[listIndex] = node->next; // if node is the first element, make the second element the start of the list
-//   if(node->next) node->next->prev = node->prev;
-// }
-
-metadata_t* find_fit(metadata_t *header, size_t size)
-{
-  metadata_t* ptr = header;
-  
-  while(ptr != NULL)
-  {
-    if(ptr->size >= (size + sizeof(metadata_t)) && ptr->available == 1)
-    {
-      return ptr;
-    }
-    lastVisited = ptr;
-    ptr = ptr->next;
-  }  
-  return ptr;  
-}
-
-void splitChunk(metadata_t* ptr, unsigned int numbytes)
+void split_chunk(metadata_t *ptr, size_t numbytes)
 {
   metadata_t *newChunk = NULL; 
   
@@ -149,6 +70,45 @@ void splitChunk(metadata_t* ptr, unsigned int numbytes)
   ptr->size = numbytes;
   ptr->available = 0;
   ptr->next = newChunk;
+}
+
+
+metadata_t* extendH(metadata_t *lastVisitedPtr, size_t size)
+{
+  bp0 = sbrk(0);
+  metadata_t *curBreak = bp0;    //Current breakpoint of the heap
+  
+  if(sbrk(size + METADATA_T_ALIGNED) == (void*) -1)
+  {
+    return NULL;
+  }
+  
+  curBreak->size = size + METADATA_T_ALIGNED - sizeof(metadata_t);
+  curBreak->available = 0;
+  curBreak->next = NULL;
+  curBreak->prev = lastVisitedPtr;
+  lastVisitedPtr->next = curBreak;
+  
+  if(curBreak->size > size)
+    split_chunk(curBreak, size);
+  
+  return curBreak;  
+}
+
+metadata_t* find_fit(metadata_t *header, size_t size)
+{
+  metadata_t* ptr = header;
+  
+  while(ptr != NULL)
+  {
+    if(ptr->size >= (size + sizeof(metadata_t)) && ptr->available == 1)
+    {
+      return ptr;
+    }
+    lastVisited = ptr;
+    ptr = ptr->next;
+  }  
+  return ptr;  
 }
 
 
@@ -190,10 +150,8 @@ void *dmalloc(size_t numbytes) {
   assert(numbytes > 0);
 
   
-  printf("debugg\n");
-  metadata_t *ptr;
   
-
+  metadata_t *ptr;
   //Split the chunk into two: one with size request by user, other with the remainder.
   ptr = freelist;
     
@@ -201,18 +159,24 @@ void *dmalloc(size_t numbytes) {
   // find the first free heap chunk that is big enough
   metadata_t *smallest_chunk = NULL; //first chunk encountered that is big enough
   smallest_chunk = find_fit(freelist, numbytes);
+ 
 
-  if(smallest_chunk != NULL){
-
+  if(smallest_chunk == NULL){
+      //extend the heap 
+      smallest_chunk = extendH(lastVisited, numbytes);
+      return smallest_chunk->end;
+    }
+  else{
     // cut the chunk down to a reasonable size
     if(smallest_chunk->size > numbytes){
-      splitChunk(smallest_chunk, numbytes);
+      split_chunk(smallest_chunk, numbytes);
+      }
     }
-  }
-  return smallest_chunk;
+  
+  return smallest_chunk->end;
 }
 
-void mergeChunkPrev(metadata_t *freed)
+void coalesce_prev(metadata_t *freed)
 { 
   metadata_t *prev;
   prev = freed->prev;
@@ -226,11 +190,11 @@ void mergeChunkPrev(metadata_t *freed)
   }
 }
 
-/* mergeChunkNext: merge one freed chunk with the following chunk (in case it is free as well)
+/* coalesce_next: merge one freed chunk with the following chunk (in case it is free as well)
      chunkStatus* freed: pointer to the block of memory to be freed.
      retval: void, the function modifies the list
 */
-void mergeChunkNext(metadata_t *freed)
+void coalesce_next(metadata_t *freed)
 {  
   metadata_t *next;
   next = freed->next;
@@ -248,55 +212,14 @@ void dfree(void *ptr) {
   metadata_t *toFree = NULL;
   toFree = ptr - sizeof(metadata_t);
   
-  if(toFree >= freelist && toFree <= (metadata_t *)bp0)
+  if(toFree >= freelist && toFree <= (size_t *)bp0)
   {
     toFree->available = 1;  
-    mergeChunkNext(toFree);
-    mergeChunkPrev(toFree);
+    coalesce_next(toFree);
+    coalesce_prev(toFree);
   
   }
 }
-
-
-//   if(!ptr) return;
-//   metadata_t *header = hdr_for_payload(ptr);
-//   size_t size = header->size;
-//   metadata_t *node = (metadata_t*) ptr;
-
-//   //coalescing
-//   metadata_t *next_header = (metadata_t*)((char*)ptr + size);
-//   // while the addr of the next header is free and within the current heap segment
-//   while(next_header->free){
-//     metadata_t *nextNode = payload_for_hdr(next_header);
-//     int listIndex = get_right_list(next_header->size);
-//     remove_from_list(nextNode, listIndex);
-//     header->size += next_header->size + sizeof(metadata_t);
-//     next_header = (metadata_t*) ((char*)ptr + header->size);
-//   }
-
-//   int listIndex = get_right_list(header->size);
-//   header->free = true;
-//   add_to_list(node, listIndex);
-// }
-
-
-
-//  */
-// static void print_free(){
-//   printf("Current State of freed lists: \n");
-//   for(int i = 0; i < NUM_LISTS; i ++){
-//     printf("[%d]: ", i);
-//     metadata_t *cur = list_arr[i];
-//     while(cur){
-//       printf("%#x -> ", (int)cur);
-//       cur = cur->next;
-//     }
-//     printf("NULL\n");
-//   }
-//   printf("\n");
-// }
-
- 
 
 /* for debugging; can be turned off through -NDEBUG flag*/
 void print_freelist() {
