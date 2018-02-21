d9a7070ef5fe89f8df30e214e69122515c16dc36
Zhengzhi Xu <zhengzhixu@Zhengzhis-MacBook-Pro.local>
2018-02-20 22:10:17 -0500

    created struct tcb

--- p1t/thread.cc
+++ p1t/thread.cc
@@ -8,14 +8,31 @@
 
 using namespace std;
 
+int THREAD_COUNT = 0; //total number of thread
 
-struct Thread{
+struct TCB{
 	// name/status (TCB) -> stack , ucontext_t
-	char* stack;
-	int id;
-	int status;
-	ucontext_t* ucontext;
+	int tid; // thread identifier
+	char* stack; //stack pointer 
+	int status; //state of the thread: running ready waiting start done
+	ucontext_t* ucontext; // ucontext
+	
 }
 
 int thread_create(thread_startfunc_t func, void *arg){
+	//allocate thread control block
+	//allocate stack
+	//build stack frame for base of stack (stub)
+	//put func, args on stack
+	//goes in ready queue
+	interrupt_disable();
+
+
+
+
+
+
+
+
+	interrupt_enable();
 }
