403c1af7a5226a2b4dd84f2513108818daddcc3e
Zhengzhi Xu <zx32@login-teer-13.oit.duke.edu>
2018-02-24 20:13:51 -0500

    test2 not looping

---
+++ p1t/test2.cc
@@ -0,0 +1,126 @@
+#include <stdlib.h>
+#include <iostream>
+#include "thread.h"
+#include <assert.h>
+//test ping pong
+
+using namespace std;
+
+int lock1 = 1;
+int cond1 = 1;
+int lock2 = 2;
+
+void pingpong1(void* arg){
+  //ping pong  while something is not done, mx acquire, print, mx release
+  if (thread_lock(lock1) < 0) { //thread 1 should get this lock first
+    cout << "thread lock1 failed\n";
+    exit(0);
+  }else{
+    cout << "thread lock1 successful\n";
+  }
+
+  //thread 1 get this lock, ping and relinguish the lock and waits, in CV queue now
+  while(true){
+    cout << "ping\n";
+    if (thread_wait(lock1, cond1) < 0){
+      cout << "thread1 wait failed\n";
+      exit(0);
+    }else{
+      cout << "thread1 waits\n";
+    }
+
+    if (thread_signal(lock1, cond1) < 0){ //signal and thread1 is on ready
+      cout << "thread1 signal failed\n";
+      exit(0);
+    } else{
+      cout << "thread1 signal succeeded\n";
+    }
+    if (thread_yield() < 0){//signal and thread 1 is running
+      cout << "thread1 yield failed\n";
+      exit(0);
+    }else{
+      cout << "thread1 yield succeeded\n";
+    }
+    //returns after thread 1 yields
+  }
+
+  if (thread_unlock(lock1) < 0) {
+    cout << "thread unlock1 failed\n";
+    exit(0);
+  }else{
+    cout << "thread unlock1 successful\n";
+  }
+  
+}
+
+void pingpong2(void* arg){
+  //ping pong  while something is not done, mx acquire, print, mx release
+
+  if (thread_lock(lock1) < 0) { //thread 1 waits, thread2 gets this lock
+    cout << "thread lock1 failed\n";
+    exit(0);
+  }else{
+    cout << "thread lock1 successful\n";
+  }
+
+  //thread pongs and put herself on wait(), thread 2 in CV queue now
+  while (true){
+    cout << "pong\n";
+    if (thread_wait(lock1, cond1) < 0){
+      cout << "thread2 wait failed\n";
+      exit(0);
+    }else{
+      cout << "thread2 waits\n";
+    }
+
+    if (thread_signal(lock1, cond1) < 0){ //signal and thread1 is on ready
+      cout << "thread2 signal failed\n";
+      exit(0);
+    } else{
+      cout << "thread2 signal succeeded\n";
+    }
+    if (thread_yield() < 0){//signal and thread 1 is running
+      cout << "thread2 yield failed\n";
+      exit(0);
+    }else{
+      cout << "thread2 yield succeeded\n";
+    }
+    //returns after thread 1 yields
+  }
+
+  if (thread_unlock(lock1) < 0) {
+    cout << "thread unlock1 failed\n";
+    exit(0);
+  }else{
+    cout << "thread unlock1 successful\n";
+  }
+  
+}
+
+void parent(){
+  cout << "thread enters parent\n";
+  if (thread_create((thread_startfunc_t) pingpong1, (void*) 100) < 0){
+    cout << "\nthread 1 failed";
+    exit(0);
+  }else{
+    cout << "\nthread 1 created";
+  }
+  if (thread_create((thread_startfunc_t) pingpong2, (void*) 100) < 0){
+    cout << "\nthread 2 failed";
+    exit(0);
+  }else{
+    cout << "\nthread 2 created";
+  }
+  
+
+}
+
+
+int main() {
+  if (thread_libinit( (thread_startfunc_t) parent, (void *) 100)) {
+    cout << "thread_libinit failed\n";
+    exit(1);
+  }else{
+    cout << "thread_libinit susceeded\n";
+  }
+}
