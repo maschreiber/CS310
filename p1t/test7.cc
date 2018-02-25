9bd7b0713d4f454c51b49631c3dbcb96dcd04464
Zhengzhi Xu <zx32@login-teer-13.oit.duke.edu>
2018-02-24 22:40:13 -0500

    add test8 for libinit multiple calls

--- p1t/test7.cc
+++ p1t/test7.cc
@@ -3,33 +3,60 @@
 #include "thread.h"
 #include <assert.h>
 using namespace std;
-//broadcast does not sent every thread to ready queue
-//put all ready thread in the 
+//tests if broadcast does not sent every thread to ready queue
+
+//put all ready thread in the cv queue
 //broadcast: put everything on the cv wait queue to the tail of the ready queue
 //make each thread run after broadcast and couts when thread starts from broadcast
 
 
-int lock = 1;
-int cond = 1;
+int lock1 = 1;
+int cond1 = 1;
+
+
+
+
+
+void block(void* arg){
+	if (thread_lock(lock1) < 0) { 
+    	cout << "thread lock1 failed.\n";
+  	}else{
+  		cout << "thread lock1 successful.\n";
+  	}
+
+  	thread_yield();
+
+  	if (thread_unlock(lock1) < 0) { 
+    	cout << "thread unlock1 failed.\n";
+  	}else{
+  		cout << "thread unlock1 successful.\n";
+  	}
+
+
+}
+
+void broadcast(void* arg){
+	
+}
+
 
 
 
 void parent(void* arg){
 	cout << "thread enters parent\n";
-  if (thread_create((thread_startfunc_t) one, (void*) 100) < 0){
+  if (thread_create((thread_startfunc_t) broadcast, (void*) 100) < 0){
     cout << "thread 1 failed\n";
-    exit(0);
+    exit(1);
   }else{
     cout << "thread 1 created\n";
   }
 
-  if (thread_create((thread_startfunc_t) two, (void*) 100) < 0){
+  if (thread_create((thread_startfunc_t) broadcast, (void*) 100) < 0){
     cout << "thread 2 failed\n";
-    exit(0);
+    exit(1);
   }else{
     cout << "thread 2 created\n";
   }
-  
 }
 
 
